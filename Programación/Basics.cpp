3a7c0339fcd652417985a31dc118f7a0dca0bdc7
Jorge Baez <22-0637@copphu.unphu.edu.do>
2023-05-15 09:35:00 -0400

    Agregando clase Variables y tipos

--- Programación/Basics.cpp
+++ Programación/Basics.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int variables()
-{
-	int a{ 5 }, b{ 1 };
-
-	a = a + b;
-
-	return a;
-}
-
+//Estructura basica de un programa 001
 int main()
 {
-	int a;
-	a = variables();
 	cout << a << endl;
 	string salute = "Hello world";
 	cout << salute << endl;
 	salute = "GoodBye World!";
 	cout << salute << endl;
+}
+
+//Variables y tipos
+int variables()
+{
+	int numero = {30};
+	numero = numero + 69;
+	
+	double decimal =  99.5;
+	
+	char letra = 'a';
+	
+	string cadena = "Cadena de caracteres";
+	
+	bool booleano = false;
+	
+	auto resultado = numero + decimal;
+	
+	cout << numero << endl << decimal << endl << resultado << endl << letra << endl << cadena << endl << booleano;
+	
 }
