4535615ed1ad85cbcf237d15b913fe41c4858b0b
jbaez07 <jbaez@cloudservices.com.do>
2023-08-26 23:54:27 -0400

    stack ds

---
+++ stack.cpp
@@ -0,0 +1,16 @@
+#include <stack>
+#include <iostream>
+using namespace std;
+
+int main(){
+    //En verdad las pilas solo tienen tres funciones miembros que conozca:
+    //push, top y pop. una data structure mediocre
+    stack<int>stackoverflow;
+    stackoverflow.push(9);
+    stackoverflow.push(1);
+    stackoverflow.push(2);
+    stackoverflow.push(3);
+
+    cout << stackoverflow.top(); //3
+    stackoverflow.pop(); //Se elimina 3.
+}
