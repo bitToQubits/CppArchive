c2655ac16d6706187678da6202fd1e4aede2bd58
jbaez07 <jbaez@cloudservices.com.do>
2023-09-20 21:18:12 -0400

    Comentario

--- binarios.cpp
+++ binarios.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
+//Si n < 2 imprimimos n (que sera 1), en caso contrario imprimimos
+//1 si n es impar, 0 si n es par, cuando la recursion regrese de 1 a n
 void imprime_binario ( int n ) {
     if(n>=2){
         imprime_binario( n /2 );
         printf ("%d",n%2);
     } else {
-        printf ("%d",n) ;
+        printf ("%d",n);
     }
 }
 
